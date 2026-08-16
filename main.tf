9d53b4bc6aeeb87c224bd0fe56ff8065c1b7e4bc
Tanvir-Alam625 <devtanvir01@gmail.com>
2026-08-17 01:00:26 +0600

    chore: added s3 instance

--- main.tf
+++ main.tf
@@ -29,3 +29,38 @@ resource "aws_instance" "app_server" {
         Owner = "Tanvir"
     }
 }
+
+# Random Suffix to guarantee a globally unique S3 bucket name
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
+}
+
+# S3 Bucket
+resource "aws_s3_bucket" "app_bucket" {
+    bucket = "${var.bucket_name_prefix}-${random_id.bucket_suffix.hex}"
+
+    tags = {
+        Name = "${var.project_name}-${var.environment}-app-bucket"
+        Project = var.project_name
+        Environment = var.environment
+        ManagedBy = "Terraform"
+        Owner = "Tanvir"
+    }
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_ownership" {
+    bucket = aws_s3_bucket.app_bucket.id
+
+    rule {
+        object_ownership = "BucketOwnerPreferred"
+    }
+}
+
+resource "aws_s3_bucket_public_access_block" "app_bucket" {
+    bucket = aws_s3_bucket.app_bucket.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+}
